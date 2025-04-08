44d67054ad2fe5bb56752c27ff2358776e711f27
deepajadhav1107 <deepajadhav2001@gmail.com>
2025-04-08 22:07:23 +0530

    save

--- Simple.java
+++ Simple.java
@@ -3,6 +3,7 @@ public class Simple {
     public static void main(String[] args)
     {
         System.out.println("Shrusti");
-        System.out.println("Deepa";)
+        System.out.println("Deepa");
+        System.out.println("Main")
     }
 }
